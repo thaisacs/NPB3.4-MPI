a71699a7a0fe5360b5c87e9f0196776aaefcc1cd
thais <thaiscamachoo@gmail.com>
2020-06-07 14:17:02 -0300

    up

--- experiments/script-build.sh
+++ experiments/script-build.sh
@@ -6,6 +6,8 @@ cd kernel_stats
 make
 cd ..
 
+rm config/suite.def
+
 for bench in cg ep is mg ft; do
   for size in A B C D E; do
 	echo "$bench	$size" >> config/suite.def
--- experiments/script-experiment.sh
+++ experiments/script-experiment.sh
@@ -5,6 +5,6 @@ cd results
 
 for bench in cg ep is mg ft; do
   for size in A B; do
-	/usr/bin/time -o $bench-$size-TIME.out -p  mpirun -n 2 ../../bin/$bench.$size.x 1> $bench-$size.out 2> $bench-$size.err
+	/usr/bin/time -o $bench-$size-TIME.out -p  mpirun -n 1 ../../bin/$bench.$size.x 1> $bench-$size.out 2> $bench-$size.err
   done
 done
