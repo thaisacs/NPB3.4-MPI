737074f7f4f4a4a15e159d7ee32927c8fa111083
Thais <thaiscamachoo@gmail.com>
2020-06-15 18:03:09 -0300

    results

--- experiments/script-exp.sh
+++ experiments/script-exp.sh
@@ -2,11 +2,25 @@
 
 mkdir -p results
 
-for bench in bt lu cg sp ft; do
-  for size in A B C; do
+for bench in lu cg ft; do
+  for size in B C; do
 	for threads in 2 4 8; do
 		 /usr/bin/time -o results/$bench-$size-TIME-$threads.out -p  mpirun -n $threads ../bin/$bench.$size.x 1> results/$bench-$size.$threads.out 2> results/$bench-$size.$threads.err
 	done
+  done
+done
+
+for bench in bt sp; do
+  for size in B C; do
+	for threads in 4; do
+		 /usr/bin/time -o results/$bench-$size-TIME-$threads.out -p  mpirun -n $threads ../bin/$bench.$size.x 1> results/$bench-$size.$threads.out 2> results/$bench-$size.$threads.err
+	done
+  done
+done
+
+for bench in lu cg ft sp bt; do
+  for size in B C; do
+	  /usr/bin/time -o results/$bench-$size-TIME-S.out -p ../bin/$bench.$size.x 1> results/$bench-$size.S.out 2> results/$bench-$size.S.err
   done
 done
 
