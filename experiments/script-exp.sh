94341c7226a1d19bc4bb99cdbf70dd8ebbed27d9
Thais <thaiscamachoo@gmail.com>
2020-06-15 18:11:38 -0300

    clean

--- experiments/script-exp.sh
+++ experiments/script-exp.sh
@@ -2,21 +2,21 @@
 
 mkdir -p results
 
-for bench in lu cg ft; do
-  for size in B C; do
-	for threads in 2 4 8; do
-		 /usr/bin/time -o results/$bench-$size-TIME-$threads.out -p  mpirun -n $threads ../bin/$bench.$size.x 1> results/$bench-$size.$threads.out 2> results/$bench-$size.$threads.err
-	done
-  done
-done
-
-for bench in bt sp; do
-  for size in B C; do
-	for threads in 4; do
-		 /usr/bin/time -o results/$bench-$size-TIME-$threads.out -p  mpirun -n $threads ../bin/$bench.$size.x 1> results/$bench-$size.$threads.out 2> results/$bench-$size.$threads.err
-	done
-  done
-done
+#for bench in lu cg ft; do
+#  for size in B C; do
+#	for threads in 2 4 8; do
+#		 /usr/bin/time -o results/$bench-$size-TIME-$threads.out -p  mpirun -n $threads ../bin/$bench.$size.x 1> results/$bench-$size.$threads.out 2> results/$bench-$size.$threads.err
+#	done
+#  done
+#done
+#
+#for bench in bt sp; do
+#  for size in B C; do
+#	for threads in 4; do
+#		 /usr/bin/time -o results/$bench-$size-TIME-$threads.out -p  mpirun -n $threads ../bin/$bench.$size.x 1> results/$bench-$size.$threads.out 2> results/$bench-$size.$threads.err
+#	done
+#  done
+#done
 
 for bench in lu cg ft sp bt; do
   for size in B C; do
