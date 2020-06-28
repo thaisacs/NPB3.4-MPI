0e447c8e228eb557be7c2a40fe95454c14ccef6a
Thais <thaiscamachoo@gmail.com>
2020-06-28 16:55:24 -0300

    results clean

--- experiments/script-exp.sh
+++ experiments/script-exp.sh
@@ -4,7 +4,7 @@ mkdir -p results
 cd results
 
 
-for bench in lu cg ft; do
+for bench in lu cg ft bt sp; do
   for size in B C; do
     mkdir -p $bench.$size
     cd $bench.$size
@@ -14,27 +14,9 @@ for bench in lu cg ft; do
       now=$(date +"%m-%d-%y-%T")
       mkdir -p $now
       cd $now
-      /usr/bin/time -o results.time -p mpirun -n $threads ../../../../../bin/$bench.$size.x 1> results.out 2> results.err
+      /usr/bin/time -o results.time -v mpirun -n $threads ../../../../../bin/$bench.$size.x 1> results.out 2> results.err
       cd ../..
     done
     cd ..
   done
 done
-
-for bench in bt sp; do
-  for size in B C; do
-    mkdir -p $bench.$size
-    cd $bench.$size
-    for threads in 1; do
-      mkdir -p ${threads}node06
-      cd ${threads}node06
-      now=$(date +"%m-%d-%y-%T")
-      mkdir -p $now
-      cd $now
-      /usr/bin/time -o results.time -p mpirun -n $threads ../../../../../bin/$bench.$size.x 1> results.out 2> results.err
-      cd ../..
-    done
-    cd ..
-  done
-done
-
